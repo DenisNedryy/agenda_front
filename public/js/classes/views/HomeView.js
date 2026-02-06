68579862f9bb71c7148d377cea957403ccc7cbd0
Denis Nedryy <denis.nedryy@gmail.com>
2026-02-06 22:15:26 +0100

    sdfdf

--- public/js/classes/views/HomeView.js
+++ public/js/classes/views/HomeView.js
@@ -19,66 +19,7 @@ export class HomeView {
                     </div>
                 </div>
 
-                <div class="home__webSites box">
-                    <p class="home__webSites--title">Sites webs</p>
-                    <div class="home__webSites--container">
-                        <a href="https://www.youtube.com/" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/youtube.png"/>
-                                 <p>Youtube</p>
-                            </div>
-                        </a>
-
-                        <a href="https://chatgpt.com" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/gpt.webP"/>
-                                 <p>Gpt</p>
-                            </div>
-                        </a>
-                        <a href="https://codepen.io/pen" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/codepen.png"/>
-                                 <p>Code-pen</p>
-                            </div>
-                        </a>
-
-                        <a href="https://www.greatfrontend.com" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/gfe.png"/>
-                                 <p>GFE</p>
-                            </div>
-                        </a>
-
-                        <a href="https://www.gmx.fr" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/gmx.png"/>
-                                 <p>GMX</p>
-                            </div>
-                        </a>
-
-                        <a href="https://gemini.google.com/app?hl=fr" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/gemini_logo.png"/>
-                                 <p>Gemini</p>
-                            </div>
-                        </a>
-
-                        <a href="https://aistudio.google.com/apps" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/google_ai_studio_logo.png"/>
-                                 <p>Google ai studio</p>
-                            </div>
-                        </a>
-
-                        <a href="https://antigravity.google" target="_blank">
-                             <div>
-                                <img src="/public/assets/images/icons/antigravity_logo.png"/>
-                                 <p>Antigravity</p>
-                            </div>
-                        </a>
                 
-                    </div>
-                </div>
 
                 <div class="home__bodyContainer">
                     <div class="home__bodyContainer__left">
